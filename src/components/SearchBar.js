b8dadb6e273562b355bc196a17f6165b237db23f
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2021-06-27 13:23:57 -0400

    Setting navigation and search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
+import { Feather } from '@expo/vector-icons'; 
+
+const SearchBar = () => {
+    return (
+        <View style={styles.parentStyle}>
+            <Feather name="search" size={30} color="black" style={styles.searchIconStyle}/>
+            <TextInput style={styles.searchTextInputStyle} placeholder="Search" />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    parentStyle: {
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        marginTop: 15,
+        marginHorizontal: 15,
+        borderRadius: 5,
+        flexDirection: 'row',
+    },
+    searchIconStyle: {
+        alignSelf: 'center',
+        marginHorizontal: 12
+    },
+    searchTextInputStyle: {
+        flex:1,
+        fontSize: 18
+    }
+});
+
+export default SearchBar;
