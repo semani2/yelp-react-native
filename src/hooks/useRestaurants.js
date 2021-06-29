6225ed2e47e7e3f465720464a48c86caadc4ec3a
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2021-06-28 22:12:11 -0400

    Creating a reusable hook for yelp api

---
+++ src/hooks/useRestaurants.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import yelp from '../api/yelp';
+
+export default () => {
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async (term) => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    term: term,
+                    location: 'Toronto, Canada',
+                    limit: 50
+                }
+            });
+            console.log(response.status);
+            response.data.businesses.forEach(business => {
+               console.log(business.name); 
+            });
+            setResults(response.data.businesses);
+        } catch (err) {
+            console.log(err);
+            setErrorMessage('Something went wrong! Try again later.')
+        }
+        
+    }
+
+    // Call searchApi when component is first rendered
+    // Calls the enclosed method only once
+    useEffect(() => {
+        searchApi('Breakfast');
+    }, []);
+
+    return [searchApi, results, errorMessage];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,40 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useRestaurants from '../hooks/useRestaurants';
 
 const SearchScreen = () => {
     const [searchTerm, setSearchTerm] = useState('');
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const searchApi = async (term) => {
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    term: term,
-                    location: 'Toronto, Canada',
-                    limit: 50
-                }
-            });
-            console.log(response.status);
-            response.data.businesses.forEach(business => {
-               console.log(business.name); 
-            });
-            setResults(response.data.businesses);
-        } catch (err) {
-            console.log(err);
-            setErrorMessage('Something went wrong! Try again later.')
-        }
-        
-    }
-
-    // Call searchApi when component is first rendered
-    // Calls the enclosed method only once
-    useEffect(() => {
-        searchApi('Breakfast');
-    }, []);
-
+    const [searchApi, results, errorMessage] = useRestaurants();
+   
     return (
         <View 
             style={{backgroundColor: 'white', ...StyleSheet.absoluteFillObject}}>
