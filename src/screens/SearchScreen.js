e5692488f0f15c5b9cd2c28fc914d1f6cf180d86
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2021-06-28 22:06:37 -0400

    Using useEffect hook to call api on render

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,26 +1,39 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
 
 const SearchScreen = () => {
-    const [searchTerm, setSearchTerm] = useState("");
+    const [searchTerm, setSearchTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const searchApi = async () => {
-        const response = await yelp.get('/search', {
-            params: {
-                term: searchTerm,
-                location: 'Toronto, Canada',
-                limit: 50
-            }
-        });
-        console.log(response.status);
-        response.data.businesses.forEach(business => {
-           console.log(business.name); 
-        });
-        setResults(response.data.businesses);
+    const searchApi = async (term) => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    term: term,
+                    location: 'Toronto, Canada',
+                    limit: 50
+                }
+            });
+            console.log(response.status);
+            response.data.businesses.forEach(business => {
+               console.log(business.name); 
+            });
+            setResults(response.data.businesses);
+        } catch (err) {
+            console.log(err);
+            setErrorMessage('Something went wrong! Try again later.')
+        }
+        
     }
+
+    // Call searchApi when component is first rendered
+    // Calls the enclosed method only once
+    useEffect(() => {
+        searchApi('Breakfast');
+    }, []);
 
     return (
         <View 
@@ -28,9 +41,11 @@ const SearchScreen = () => {
             <SearchBar 
                 searchTerm={searchTerm} 
                 onSearchTermChange={setSearchTerm}
-                onSearchSubmit={searchApi}/>
+                onSearchSubmit={() => searchApi(searchTerm)}/>
 
             <Text>We have found {results.length} results</Text>
+
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
         </View>
     );
 };
